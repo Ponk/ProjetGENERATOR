19497743694d98b28729d7d852e70558b1f4612c
Valentin <valentin.carle@viacesi.fr>
2013-11-25 09:37:23 +0100

    Ajout classe CAD

---
+++ WCF/CAD.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace WCF
+{
+    public class CAD
+    {
+        SqlConnection maConnexion = null;
+        string login = "user";
+        string mdp = "user123";
+
+        public CAD()
+        {
+           
+        }
+
+        public string connexion()
+        {
+
+
+            return "Connexion réussie";
+        }
+
+
+    }
+}
