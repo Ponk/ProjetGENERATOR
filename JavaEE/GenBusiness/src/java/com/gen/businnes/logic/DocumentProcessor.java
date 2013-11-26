fa0d68e49698fd16dbcd2516c8a53ae9afba1576
AntoineBenard <antoine.benard@viacesi.fr>
2013-11-26 14:27:49 +0100

    Ajout partie java antoine post problem

--- JavaEE/GenBusiness/src/java/com/gen/businnes/logic/DocumentProcessor.java
+++ JavaEE/GenBusiness/src/java/com/gen/businnes/logic/DocumentProcessor.java
@@ -7,13 +7,11 @@ package com.gen.businnes.logic;
 
 import business.domain.Document;
 import com.gen.data.cad.CAD;
-import static java.lang.Math.floor;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.StringTokenizer;
 import java.util.logging.Level;
